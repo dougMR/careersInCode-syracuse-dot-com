faff59670f085510038937ed4fd99c9b55cd14e5
Doug R <1236472+dougMR@users.noreply.github.com>
2019-04-09 16:36:03 -0400

    added font files

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('search-icon').addEventListener('click', function(){
+    alert ("clicked search icon");
+    document.getElementsByClassName('search-bar')[0].style.display = 'block';
+})
